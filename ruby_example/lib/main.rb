cdcda192560c1665674b0cec908a014305e4b8f7
Fedor Tyurin <fedor@enablebanking.com>
2021-01-16 21:38:13 +0200

    Ruby example & small improvements

---
+++ ruby_example/lib/main.rb
@@ -0,0 +1,113 @@
+require 'bundler/setup'
+
+require 'faraday'
+require 'json'
+require 'jwt'
+require 'pp'
+require 'time'
+
+def main
+  # Reading and parsing config file
+  config_file = File.read("../config.json")
+  config = JSON.parse(config_file)
+
+  # Loading RSA private key
+  key_path = "../" + config["keyPath"]
+  rsa_key = OpenSSL::PKey::RSA.new(File.read(key_path))
+
+  # Creating JWT
+  iat = Time.now.to_i
+  jwt_header = { typ: "JWT", alg: "RS256", kid: config["applicationId"] }
+  jwt_body = { iss: "enablebanking.com", aud: "api.tilisy.com", iat: iat, exp: iat + 3600 } 
+  jwt = JWT.encode(jwt_body, rsa_key, 'RS256', jwt_header)
+
+  # Requesting application details
+  r = Faraday.get("https://api.tilisy.com/application", nil, "Authorization" => "Bearer #{jwt}")
+  if r.status == 200
+    app = JSON.parse(r.body)
+    puts "Application details:"
+    puts pp(app)
+  else
+    puts "Error response #{r.status}:", r.body
+  end
+
+  # Requesting available ASPSPs
+  r = Faraday.get("https://api.tilisy.com/aspsps", nil, "Authorization" => "Bearer #{jwt}")
+  if r.status == 200
+    aspsps = JSON.parse(r.body)["aspsps"]
+    puts "Available ASPSPs:"
+    puts pp(aspsps)
+  else
+    puts "Error response #{r.status}:", r.body
+  end
+
+  # Starting authorization"
+  valid_until = Time.now + 2*7*24*60*60 # 2 weeks
+  body = {
+    access: { valid_until: valid_until.utc.iso8601 },
+    aspsp: { name: "Nordea", country: "FI" }, # { name: aspsps[0]["name"], country: aspsps[0]["country"] },
+    state: "random",
+    redirect_url: app["redirect_urls"][0]
+  }
+  headers = {
+    "Content-Type" => "application/json",
+    "Authorization" => "Bearer #{jwt}"
+  }
+  r = Faraday.post("https://api.tilisy.com/auth", JSON.dump(body), headers)
+  if r.status == 200
+    auth_url = JSON.parse(r.body)["url"]
+    puts "To authenticate open URL #{auth_url}"
+  else
+    puts "Error response #{r.status}:", r.body
+  end
+
+  # Reading auth code and creating user session
+  puts "Enter value of code parameter from the URL you were redirected to:"
+  auth_code = nil
+  ARGF.each_line do |line|
+    auth_code = line.gsub(/\s/, "")
+    break
+  end
+  body = { code: auth_code }
+  r = Faraday.post("https://api.tilisy.com/sessions", JSON.dump(body), headers)
+  if r.status == 200
+    session = JSON.parse(r.body)
+    puts "New user session has been created:"
+    puts pp(session)
+  else
+    puts "Error response #{r.status}:", r.body
+  end
+
+  # Retrieving account balances
+  account_uid = session["accounts"][0]["uid"]
+  r = Faraday.get(
+    "https://api.tilisy.com/accounts/#{account_uid}/balances",
+    nil,
+    "Authorization" => "Bearer #{jwt}"
+  )
+  if r.status == 200
+    balances = JSON.parse(r.body)["balances"]
+    puts "Balances:"
+    puts balances
+  else
+    puts "Error response #{r.status}:", r.body
+  end
+
+  # Retrieving account transactions (since yesterday)
+  r = Faraday.get(
+    "https://api.tilisy.com/accounts/#{account_uid}/transactions",
+    { date_from: Date.today.prev_day.iso8601 },
+    "Authorization" => "Bearer #{jwt}"
+  )
+  if r.status == 200
+    transactions = JSON.parse(r.body)["transactions"]
+    puts "Transactions:"
+    puts transactions
+  else
+    puts "Error response #{r.status}:", r.body
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  main
+end
